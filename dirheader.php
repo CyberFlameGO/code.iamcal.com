fe869c408ba4cef1582910123c3c574395f723c4
Cal Henderson <cal@iamcal.com>
2011-07-24 02:09:10 +0000

    show header nav bar on file indexes

---
+++ dirheader.php
@@ -0,0 +1,8 @@
+<? include('/var/www/cal/iamcal.com/templates/universal_nav.txt'); ?>
+<style>
+body { margin: 0; padding: 0}
+#content { padding: 10px; }
+</style>
+<div id="content">
+
+<h1>Index of <?=HtmlSpecialChars($_SERVER['REQUEST_URI'])?></h1>
