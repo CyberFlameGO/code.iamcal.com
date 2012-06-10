2c6432a6d2ea0d5a337cb4f63d3baa110828a98c
Cal Henderson <cal@iamcal.com>
2012-06-10 22:22:31 +0000

    format dir listing pages like the other code pages

--- dirheader.php
+++ dirheader.php
@@ -1,8 +1,28 @@
-<? include('/var/www/cal/iamcal.com/templates/universal_nav.txt'); ?>
+<?
+	$nav = 'code';
+	include('/var/www/cal/iamcal.com/templates/universal_nav.txt');
+?>
 <style>
-body { margin: 0; padding: 0}
-#content { padding: 10px; }
+
+body {
+	text-align: center;
+	margin: 0;
+	padding: 0;
+	background-color: #f5f5f5;
+}
+
+#main {
+	width: 800px;
+	margin: 0 auto;
+	text-align: left;
+	padding: 1em 1em 5em 1em;
+	border-left: 1px solid #999;
+	border-right: 1px solid #999;
+	background-color: #fff;
+	font-family: Helvetica, Arial, sans-serif;
+}
+
 </style>
-<div id="content">
+<div id="main">
 
 <h1>Index of <?=HtmlSpecialChars($_SERVER['REQUEST_URI'])?></h1>
