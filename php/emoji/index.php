21d55f394a561bb4eafd4f04ffe9ab4bb66e589c
Cal Henderson <cal@iamcal.com>
2016-02-16 11:34:12 -0600

    no need for a submodule here

--- php/emoji/index.php
+++ php/emoji/index.php
@@ -8,6 +8,8 @@
 
 <p>This library allows the handling and conversion of Emoji in PHP.</p>
 
+<p>For background, you might want to <a href="http://www.iamcal.com/emoji-in-web-apps/">read this</a> first.</p>
+
 <p>You can <a href="https://github.com/iamcal/php-emoji/archive/master.zip">download a zipfile</a> of the latest code,
 	which contains a helpful readme file.</p>
 
@@ -33,69 +35,7 @@
 	# now we want to show it in a desktop browser
 
 	$html = emoji_unified_to_html($clean_text);
-?&gt;
 </pre>
-
-
-<h2>WTF is this crap?</h2>
-
-<p>Ahh, young grasshopper. You might want to <a href="http://www.iamcal.com/emoji-in-web-apps/">read this</a> first.</p>
-
-
-<style>
-
-pre {
-
-	border: 1px solid #666;
-
-	background-color: #EEE;
-
-	padding: 10px;
-
-}
-
-table {
-    -webkit-border-radius: 0.41em;
-    -moz-border-radius: 0.41em;
-    border: 1px solid #999;
-    font-size: 12px;
-    width: 100%;
-}
-
-table td {
-    padding-left: 0.41em;
-    padding-right: 0.41em;
-}
-
-table th {
-    font-weight: bold;
-    text-align: left;
-    background: #BBB;
-    color: #333;
-    font-size: 14px;
-    padding: 0.41em;
-}
-
-table tbody tr:nth-child(even) {
-    background: #dedede;
-}
-
-table tbody td {
-    padding: 0.41em;
-}
-
-<? include('php-emoji/emoji.css'); ?>
-
-</style>
-
-<h2>Full Emoji Catalog</h2>
-
-<?
-	$all = file_get_contents('php-emoji/table.htm');
-	if (preg_match('!<table.*?\/table>!s', $all, $m)){
-		echo $m[0];
-	}
-?>
 
 <?
 	include('../../foot.txt');
