8f1752257ee5ced84c91bec6400706722a2f0741
Cal Henderson <cal@iamcal.com>
2018-01-11 05:18:35 +0000

    recursive clone is now part of the install runner

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd /var/www/html/code.iamcal.com
-git submodule update --init --recursive
 ln -s /var/www/html/code.iamcal.com/site.conf /etc/apache2/sites-available/code.iamcal.com.conf
 a2ensite code.iamcal.com
 service apache2 reload
